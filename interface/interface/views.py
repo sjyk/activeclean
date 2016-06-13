9d9190a38207b817fa7cb4f164fe4077a046436b
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-06-13 13:35:21 -0700

    Updated

--- interface/interface/views.py
+++ interface/interface/views.py
@@ -3,7 +3,8 @@ from django.template import loader, RequestContext, Context, Template
 import json
 from interface import settings
 import unicodedata
-
+import petl as etl
+from chardet.universaldetector import UniversalDetector
 
 """
 This loads the home page
@@ -17,23 +18,56 @@ def sanitize(line):
 	return unicodedata.normalize('NFKD', unicode(line.strip(),errors='replace')).encode('ascii','ignore').encode('string_escape')
 
 """
-This loads a sample of data from the file
+This loads a sample of data from the file, 
+
+!!fix header 
 """
 def load(request):
 	filename = request.GET.get('name','')
 	fullpath = settings.DATA_DIR+filename
-
-	n = 25
-	f = open(fullpath,'rb+')
-	line = f.readline()
-	data = []
-	while line != "" and n > 0:
-		data.append(sanitize(line))
-		line = f.readline() 
-		n = n - 1
-
-	print fullpath
+	"""
+	detector = UniversalDetector()
+	file_open = open(fullpath)
+	for line in file_open.readlines():
+		detector.feed(line)
+		if detector.done: break
+		detector.close()
+	file_open.close()
+	"""
+	encoding = 'ascii'#detector.result['encoding']
 
 	response_data = {}
-	response_data['result'] = data
+	a = tryExtractors(fullpath, encoding)
+	response_data['result'] = [row for row in etl.head(a)]
+	response_data['headers'] = etl.header(a)
+	typeInference(a)
 	return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def loadCSV(filname, encoding, delimiter=','):
+	print encoding
+	table2 = etl.fromcsv(filname,delimiter=delimiter, encoding=encoding)
+	return table2
+
+#first row header fix
+
+def tryExtractors(filename, encoding, delimiter_list=[',','\t', ';', '|'], quality_metric= lambda x: len(x)):
+	result = []
+	for d in delimiter_list:
+		csvView = loadCSV(filename, encoding, delimiter=d)
+		result.append((quality_metric(etl.header(csvView)), csvView))
+	result.sort()
+	return cleanFormatTable(result[-1][1])
+
+def cleanFormatTable(table):
+	newtable = table
+	for h in etl.header(table):
+		newtable = etl.convert(table, h, sanitize)
+	return newtable
+
+def typeInference(table):
+	for h in etl.header(table):
+		col =  etl.cut(table, h)
+		print etl.nrows(col)
+
+
--- interface/static/interface/js/dashboard.js
+++ interface/static/interface/js/dashboard.js
@@ -20,16 +20,62 @@ function loadFile() {
       $('.load-start').fadeOut();
       $('.extract-start').fadeIn();
 
+      if (data['headers'].length < 5)
+        $('#col-scroll').hide();
+
+
       var table = "<table class=\"pure-table pure-table-striped\">";
-      table = table + "<thead><tr><th>Sample Data</th></tr></thead><tbody>";
+
+      table = table + "<thead><tr>";
+      for (var i = 0, len = data['headers'].length; i < len; i++)
+      { 
+        if (i < 5)
+          table = table + "<th class=\"head-col-"+i+"\"><a href=\"#\" onclick=\"editAttr("+i+");\">"+i+"</a></th>"
+        else
+          table = table + "<th class=\"head-col-"+i+"\" style=\"display:none;\"><a href=\"#\" onclick=\"editAttr("+i+");\">"+i+"</a></th>"
+      }
+
+      table = table + "</tr></thead><tbody>";
 
       for (var i = 0, len = data['result'].length; i < len; i++) {
-  			table = table + "<tr><td style=\"max-width:800px;\">" + data['result'][i] + "</td></tr>";
+        
+        table = table + "<tr>";
+        for (var j = 0, len2 = data['result'][i].length; j < len2; j++)
+           {
+              //!hard coded fix
+              if (j < 5)
+                table = table + "<td class=\"data-col-"+j+"\">"+data['result'][i][j]+"</td>";
+              else
+                table = table + "<td class=\"data-col-"+j+"\" style=\"display:none;\">"+data['result'][i][j]+"</td>";
+
+           }  
+
+  			table = table + "</tr>";
 		}
 
       $('.extract-data').html(table+"</tbody></table>")
     }
   });
+  window.cur_col = 0;
 }
 
+//fix
+function scroll() {
+   for (var j = window.cur_col, len2 = window.cur_col+5; j < len2; j++)
+   {
+       $(".data-col-"+j).hide();
+       $(".head-col-"+j).hide();
+   }
+   for (var j = window.cur_col+5, len2 = window.cur_col+10; j < len2; j++)
+   {
+      $(".data-col-"+j).show();
+      $(".head-col-"+j).show();
+   }
+   window.cur_col = window.cur_col + 5;
+}
 
+//fix
+function editAttr(j) {
+   $('.col-editor').show()
+   $('#attrname').val(j)
+}
